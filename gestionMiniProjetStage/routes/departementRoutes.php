0c645cf95b68de49e6dd9f480fc7c7e860265ce6
ayoubhakik <ayoubhakikhakik@gmail.com>
2020-04-06 20:11:37 +0100

    folders devided

--- gestionMiniProjetStage/routes/departementRoutes.php
+++ gestionMiniProjetStage/routes/departementRoutes.php
@@ -2,6 +2,7 @@
 
 Route::get('/departement/home',function(){
     return view('Departement/home');
+
 });
 Route::get('/about',function(){
     return view('about');
--- gestionMiniProjetStage/routes/web.php
+++ gestionMiniProjetStage/routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 include "departementRoutes.php";
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
