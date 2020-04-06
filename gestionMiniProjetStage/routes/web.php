f354bf248a0f72e71dcf93f3fc03ce1d84a73293
ayoubhakik <ayoubhakikhakik@gmail.com>
2020-04-06 20:01:12 +0100

    project structure

--- gestionMiniProjetStage/routes/web.php
+++ gestionMiniProjetStage/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+include "departementRoutes.php";
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',function(){
-    return view('welcome');
-});
-Route::get('/about',function(){
-    return view('about');
-});
